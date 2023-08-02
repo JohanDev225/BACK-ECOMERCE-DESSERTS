67be2f1657314d8e131f62fba30b31a0aa4830cf
JohanDev <johandev225@gmail.com>
2023-08-01 19:20:04 -0500

    setting Front

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import cors from 'cors';
 const app = express();
 
 //Importar las rutas
@@ -17,14 +17,16 @@ app.use(express.json());
 // No vas a permitir que el servidor reciba json anidados
 app.use(express.urlencoded({extended: false}));
 
-//permitir cors 
+//permitir cors origin headers methods
+app.use(cors());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://blissful-cravings.web.app/');
     res.header('Access-Control-Allow-Headers','*');
     //accept all methods
     res.header('Access-Control-Allow-Methods','*');
     next() 
 });
+
 
 //Vas a permitir que el servidor reciba archivos
 app.use(express.static('public'));
